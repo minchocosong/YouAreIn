83c3b7451b9e53c4238f513988365dc57981bd6b
minchoco <minchocosong@gmail.com>
2021-07-29 17:44:22 +0900

    ADD lightboxed (photo gallery)

--- js/story.js
+++ js/story.js
@@ -35,32 +35,3 @@ function navi() {
     coordType: "wgs84"
   });
 }
-
-// 사진
-
-var pswpElement = document.querySelectorAll(".pswp")[0];
-
-// build items array
-var items = [
-  {
-    src: "../images/couple.jpeg",
-    w: 600,
-    h: 400
-  },
-  {
-    src: "../images/wedding.jpeg",
-    w: 1200,
-    h: 900
-  }
-];
-
-// define options (if needed)
-var options = {
-  // optionName: 'option value'
-  // for example:
-  index: 0 // start at first slide
-};
-
-// Initializes and opens PhotoSwipe
-var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-gallery.init();
